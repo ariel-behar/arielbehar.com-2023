b9df0552dd7ce33de44c4e7deae9d6dba67a7b98
Ariel Behar <ariel.behar@hotmail.com>
2023-10-09 08:47:12 +0300

    Creating a boxGeometry that will server as the ground in the experience

--- client/src/components/Experience/Experience.tsx
+++ client/src/components/Experience/Experience.tsx
@@ -1,11 +1,22 @@
+import * as THREE from 'three'
+import { OrbitControls } from "@react-three/drei";
 import Avatar from "./Avatar";
 import Harley from "./Harley";
 
 function Experience() {
+
+    
 	return (
 		<>
+            <OrbitControls />
+            <axesHelper args={[5]} position={[0, 0, 0]} />
 			<Harley />
             <Avatar />
+
+            <mesh position-y={-0.5}  >
+                <boxGeometry args={[10, 1, 10]} />
+                <meshBasicMaterial side={THREE.DoubleSide} color={'lightblue'} />
+            </mesh>
 		</>
 	);
 }
